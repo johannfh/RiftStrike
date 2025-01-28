c463f634341b4f7211ea92920593c637c024358b
Johann Heuer <johann.f.heuer@gmail.com>
2025-01-28 22:12:59 +0100

    make camera move and remove old panel from selection area

--- Camera.gd
+++ Camera.gd
@@ -1,1 +1,12 @@
 extends Camera2D
+
+@export_range(10, 500, 1) var camera_move_speed = 150
+
+func _process(delta: float) -> void:
+	var direction = Vector2.ZERO
+	
+	direction.x = Input.get_axis("camera_left", "camera_right")
+	direction.y = Input.get_axis("camera_up", "camera_down")
+	
+	direction = direction.normalized()
+	position += direction * delta * camera_move_speed
