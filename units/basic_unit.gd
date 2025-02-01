66f5d43a36eda1609f15e946e987b09846a468ad
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-01 16:48:33 +0100

    deactivate collision among units

--- units/basic_unit.gd
+++ units/basic_unit.gd
@@ -1,30 +1,36 @@
 class_name BasicUnit
 extends Unit
 
-@export var speed: float = 70
-@onready var navigation_agent: NavigationAgent2D = $NavigationAgent
+@export var speed: float = 5000
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent
 
 func _ready() -> void:
-	navigation_agent.path_desired_distance = 2.0
-	navigation_agent.target_desired_distance = 2.0
-	navigation_agent.debug_enabled = true
+	nav_agent.path_desired_distance = 2.0
+	nav_agent.target_desired_distance = 2.0
+	nav_agent.debug_enabled = true
+	make_path(position)
 
 func _physics_process(delta: float) -> void:
+	# Pull in commands
 	if len(commands) > 0:
 		var command = commands[0]
 		if command is MovementCommand:
-			if navigation_agent.target_position != command.target:
-				navigation_agent.target_position = command.target
-			if navigation_agent.is_navigation_finished():
+			if nav_agent.target_position != command.target:
+				make_path(command.target)
+			if nav_agent.is_navigation_finished():
 				print("reached target %v!" % position)
 				commands.remove_at(0)
+	var next_path_pos = nav_agent.get_next_path_position()
+	var direction = global_position.direction_to(next_path_pos)
+	var new_velocity = direction * speed * delta
 	
-	var current_agent_position: Vector2 = position
-	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-	
-	var direction = current_agent_position.direction_to(next_path_position)
-	
-	velocity = direction * speed
-	
-	if not navigation_agent.is_navigation_finished():
+	nav_agent.velocity = new_velocity
+
+
+func _on_navigation_agent_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = velocity.move_toward(safe_velocity, 100)
+	if not nav_agent.is_navigation_finished():
 		move_and_slide()
+
+func make_path(pos: Vector2):
+	nav_agent.target_position = pos
