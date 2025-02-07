cfa4c4a0fc0ce3f649d9b0d3a1d86d8bc080b3da
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-07 10:27:59 +0100

    unit selection

--- scripts/ui/SelectionBox.cs
+++ scripts/ui/SelectionBox.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Godot;
 using Godot.Collections;
 using Riftstrike.scripts.commands;
@@ -5,16 +6,60 @@ using Riftstrike.scripts.components;
 
 namespace Riftstrike.scripts.ui {
     public partial class SelectionBox : Area2D {
+        [Signal] public delegate void OnSelectionEventHandler(Array<SelectionComponent> selections);
+
         private Panel panel;
         private CollisionShape2D collisionShape;
+
+        private bool active;
+        public bool Active {
+            get => active;
+            set {
+                // disable/enable panel visibility
+                // and collision shape collision detection
+                collisionShape.Disabled = !value;
+                panel.Visible = value;
+                // reset dimensions to zero values when inactive
+                if (!value) {
+                    SetDimensions(Vector2.Zero, Vector2.Zero);
+                }
+                // update active state
+                active = value;
+            }
+        }
 
         public override void _Ready() {
             KeyMap.GetShortcut(CommandType.Move);
             panel = GetNode<Panel>("Panel");
             collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+            Active = false;
         }
 
-        public Array<SelectionComponent> GetOverlappingSelectionComponents() {
+        private Vector2 start = Vector2.Zero;
+        private Vector2 end = Vector2.Zero;
+
+        public override void _Process(double delta) {
+            if (Input.IsActionJustPressed("left_click")) {
+                Active = true;
+                start = GetGlobalMousePosition();
+            }
+            if (Active) {
+                end = GetGlobalMousePosition();
+
+                // update dimensions to new size
+                var topLeft = start.Min(end);
+                var bottomRight = start.Max(end);
+                SetDimensions(topLeft, bottomRight - topLeft);
+            }
+            if (Input.IsActionJustReleased("left_click")) {
+                var selections = GetOverlappingSelectionComponents();
+                EmitSignal(SignalName.OnSelection, selections);
+                GD.Print(selections);
+                Active = false;
+            }
+        }
+
+        private Array<SelectionComponent> GetOverlappingSelectionComponents() {
             var overlappingAreas = GetOverlappingAreas();
             var selectionComponents = new Array<SelectionComponent>();
 
@@ -27,7 +72,9 @@ namespace Riftstrike.scripts.ui {
             return selectionComponents;
         }
 
-        public void SetState(Vector2 position, Vector2 size) {
+
+
+        private void SetDimensions(Vector2 position, Vector2 size) {
             panel.Size = size;
             (collisionShape.Shape as RectangleShape2D).Size = size;
 
