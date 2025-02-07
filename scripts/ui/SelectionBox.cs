c70502598771d3eb3a8e35961f70f3ce261874d2
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-07 09:03:44 +0100

    start on selection box logic

--- scripts/ui/SelectionBox.cs
+++ scripts/ui/SelectionBox.cs
@@ -1,10 +1,38 @@
 using Godot;
+using Godot.Collections;
 using Riftstrike.scripts.commands;
+using Riftstrike.scripts.components;
 
 namespace Riftstrike.scripts.ui {
-    public partial class SelectionBox : Node2D {
+    public partial class SelectionBox : Area2D {
+        private Panel panel;
+        private CollisionShape2D collisionShape;
+
         public override void _Ready() {
             KeyMap.GetShortcut(CommandType.Move);
+            panel = GetNode<Panel>("Panel");
+            collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+        }
+
+        public Array<SelectionComponent> GetOverlappingSelectionComponents() {
+            var overlappingAreas = GetOverlappingAreas();
+            var selectionComponents = new Array<SelectionComponent>();
+
+            foreach (var area in overlappingAreas) {
+                if (area is SelectionComponent s) {
+                    selectionComponents.Add(s);
+                }
+            }
+
+            return selectionComponents;
+        }
+
+        public void SetState(Vector2 position, Vector2 size) {
+            panel.Size = size;
+            (collisionShape.Shape as RectangleShape2D).Size = size;
+
+            panel.Position = position;
+            collisionShape.Position = position + (size / 2);
         }
     }
 }
--- scripts/ui/UnitControl.cs
+++ scripts/ui/UnitControl.cs
@@ -1,9 +1,12 @@
+using System.Buffers.Binary;
 using Godot;
 
 namespace Riftstrike.scripts.ui {
     [GlobalClass]
     public partial class UnitControl : Control {
         private SelectionBox selectionBox;
+
+        [Export] public Camera2D playerCamera;
 
         public override void _Ready() {
             selectionBox = GetNode<SelectionBox>("SelectionBox");
