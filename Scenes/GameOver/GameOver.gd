dd23fcbefd5b2e667649e6963b9d59c3c71bb193
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-09 03:15:33 +0100

    deletion

--- Scenes/GameOver/GameOver.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-# TODO: Cannot preload scene. Circular dependency in preload?
-var TITLE_SCREEN = load("res://Scenes/TitleScreen/title_screen.tscn")
-
-func _on_home_button_pressed() -> void:
-	get_tree().change_scene_to_packed(TITLE_SCREEN)
