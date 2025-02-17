9aa67e0c0331e0ba37a1ed0476e4b8a84d15525f
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-17 20:09:58 +0100

    formatting

--- units/Unit.cs
+++ units/Unit.cs
@@ -2,28 +2,28 @@ using Godot;
 
 namespace Riftstrike.units
 {
-	[GlobalClass]
-	public abstract partial class Unit : Node2D
-	{
-		[Export(PropertyHint.None, "suffix:pixels")]
-		public double SafeDistance = 300;
+    [GlobalClass]
+    public abstract partial class Unit : Node2D
+    {
+        [Export(PropertyHint.None, "suffix:pixels")]
+        public double SafeDistance = 300;
 
-		public override void _Ready()
-		{
-			base._Ready();
-			UnitManager.Instance.units.Add(this);
-		}
+        public override void _Ready()
+        {
+            base._Ready();
+            UnitManager.Instance.units.Add(this);
+        }
 
-		public override void _ExitTree()
-		{
-			UnitManager.Instance.units.Remove(this);
-			UnitManager.Instance.unitsSelected.Remove(this);
-			base._ExitTree();
-		}
-	}
+        public override void _ExitTree()
+        {
+            UnitManager.Instance.units.Remove(this);
+            UnitManager.Instance.unitsSelected.Remove(this);
+            base._ExitTree();
+        }
+    }
 
-	public interface IWalk
-	{
-		public void WalkTo(Vector2 targetPosition, bool append);
-	}
+    public interface IWalk
+    {
+        public void WalkTo(Vector2 targetPosition, bool append);
+    }
 }
