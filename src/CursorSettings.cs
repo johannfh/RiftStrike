fbd84500b76139ab6bdc6850bea9b17cd7cf600a
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-23 03:13:50 +0100

    move autoload out of namespace to see if that works

--- src/CursorSettings.cs
+++ src/CursorSettings.cs
@@ -1,51 +1,48 @@
 using Godot;
 
-namespace Riftstrike
+public partial class CursorSettings : Node
 {
-    public partial class CursorSettings : Node
+    public static CursorSettings Instance { get; private set; }
+    public override void _Ready()
     {
-        public static CursorSettings Instance { get; private set; }
-        public override void _Ready()
+        base._Ready();
+        if (Instance != null && Instance != this)
         {
-            base._Ready();
-            if (Instance != null && Instance != this)
-            {
-                QueueFree();
-                return;
-            }
-            Instance = this;
-            cursorDefault = GD.Load<Texture2D>($"{cursorPath}/cursor_default.png");
-            cursorPointer = GD.Load<Texture2D>($"{cursorPath}/cursor_pointer.png");
-            cursorSelect = GD.Load<Texture2D>($"{cursorPath}/cursor_select.png");
-            Cursor = Cursor.Default;
+            QueueFree();
+            return;
         }
-        private static readonly string cursorPath = "res://src/assets/cursors";
-        private Texture2D cursorDefault;
-        private Texture2D cursorPointer;
-        private Texture2D cursorSelect;
-        private Cursor cursor = Cursor.Default;
-        public Cursor Cursor
+        Instance = this;
+        cursorDefault = GD.Load<Texture2D>($"{cursorPath}/cursor_default.png");
+        cursorPointer = GD.Load<Texture2D>($"{cursorPath}/cursor_pointer.png");
+        cursorSelect = GD.Load<Texture2D>($"{cursorPath}/cursor_select.png");
+        Cursor = Cursor.Default;
+    }
+    private static readonly string cursorPath = "res://src/assets/cursors";
+    private Texture2D cursorDefault;
+    private Texture2D cursorPointer;
+    private Texture2D cursorSelect;
+    private Cursor cursor = Cursor.Default;
+    public Cursor Cursor
+    {
+        get => cursor;
+        set
         {
-            get => cursor;
-            set
+            cursor = value;
+            Texture2D cursorTexture = cursor switch
             {
-                cursor = value;
-                Texture2D cursorTexture = cursor switch
-                {
-                    Cursor.Pointer => cursorPointer,
-                    Cursor.Select => cursorSelect,
-                    _ => cursorDefault,
-                };
-                // hotspot is image center
-                var cursorHotspot = cursorTexture.GetSize() / 2;
-                Input.SetCustomMouseCursor(cursorTexture, hotspot: cursorHotspot);
-            }
+                Cursor.Pointer => cursorPointer,
+                Cursor.Select => cursorSelect,
+                _ => cursorDefault,
+            };
+            // hotspot is image center
+            var cursorHotspot = cursorTexture.GetSize() / 2;
+            Input.SetCustomMouseCursor(cursorTexture, hotspot: cursorHotspot);
         }
     }
-    public enum Cursor
-    {
-        Default,
-        Pointer,
-        Select,
-    }
+}
+public enum Cursor
+{
+    Default,
+    Pointer,
+    Select,
 }
