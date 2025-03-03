02a0b51247a030f2b99fb6d816f3fd7e9b7afc55
Johann Heuer <johann.f.heuer@gmail.com>
2025-03-03 21:23:46 +0100

    increase icon size and add play icon

--- src/TitleScreenUI.cs
+++ src/TitleScreenUI.cs
@@ -27,7 +27,33 @@ namespace Riftstrike
                 });
                 GetTree().ChangeSceneToPacked(SceneLoader.GameScene);
             };
+        }
 
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
+            ButtonScaleTweenHover(PlayButton, 1.3F, 0.2, 0.1);
+        }
+
+        private void ButtonScaleTweenHover(Button button, float scale, double duration)
+            => ButtonScaleTweenHover(button, scale, duration, duration);
+
+        private void ButtonScaleTweenHover(Button button, float scale, double duration, double revertDuration)
+        {
+            if (button.IsHovered())
+            {
+                Tween(button, "scale", Vector2.One * scale, duration);
+            }
+            else
+            {
+                Tween(button, "scale", Vector2.One, revertDuration);
+            }
+        }
+
+        private void Tween(GodotObject obj, NodePath property, Variant amount, double duration)
+        {
+            var tween = CreateTween();
+            tween.TweenProperty(obj, property, amount, duration);
         }
     }
 }
