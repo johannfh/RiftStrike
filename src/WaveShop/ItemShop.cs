587206b8e5c34dc140a4fe54bc5caefebd126fb5
Johann Heuer <johann.f.heuer@gmail.com>
2025-03-03 22:35:15 +0100

    icon for "Next Wave" button and export v0.6.0

--- src/WaveShop/ItemShop.cs
+++ src/WaveShop/ItemShop.cs
@@ -16,5 +16,33 @@ namespace Riftstrike.src.WaveShop
                 GetTree().ChangeSceneToPacked(SceneLoader.GameScene);
             };
         }
+
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
+            ButtonScaleTweenHover(NextWaveButton, 1.3F, 0.2, 0.1);
+        }
+
+        private void ButtonScaleTweenHover(Button button, float scale, double duration, double revertDuration)
+        {
+            if (button.ButtonPressed)
+            {
+                Tween(button, "scale", Vector2.One * ((scale - 1) / 2 + 1), duration);
+            }
+            else if (button.IsHovered())
+            {
+                Tween(button, "scale", Vector2.One * scale, duration);
+            }
+            else
+            {
+                Tween(button, "scale", Vector2.One, revertDuration);
+            }
+        }
+
+        private void Tween(GodotObject obj, NodePath property, Variant amount, double duration)
+        {
+            var tween = CreateTween();
+            tween.TweenProperty(obj, property, amount, duration);
+        }
     }
 }
