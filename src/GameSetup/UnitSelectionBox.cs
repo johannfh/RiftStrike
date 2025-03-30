5a35b42b803a401323cd7cc9b1e851133ef01f06
Johann Heuer <johann.f.heuer@gmail.com>
2025-03-30 01:08:19 +0100

    integrate icons and more ui improvements

--- src/GameSetup/UnitSelectionBox.cs
+++ src/GameSetup/UnitSelectionBox.cs
@@ -56,5 +56,28 @@ namespace Riftstrike.src.GameSetup
             TextureRect.Texture = Icon;
             AnimationPlayer.Play("RESET");
         }
+
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
+            ButtonScaleTweenHover(SelectButton, 1.3F, 0.3, 0.2);
+        }
+
+
+        private void ButtonScaleTweenHover(Button button, float scale, double duration, double revertDuration)
+        {
+            if (button.ButtonPressed)
+                Tween(button, "scale", Vector2.One * ((scale - 1) / 2 + 1), duration);
+            else if (button.IsHovered())
+                Tween(button, "scale", Vector2.One * scale, duration);
+            else
+                Tween(button, "scale", Vector2.One, revertDuration);
+        }
+
+        private void Tween(GodotObject obj, NodePath property, Variant amount, double duration)
+        {
+            var tween = CreateTween();
+            tween.TweenProperty(obj, property, amount, duration);
+        }
     }
 }
--- src/PauseMenu.cs
+++ src/PauseMenu.cs
@@ -45,6 +45,24 @@ namespace Riftstrike.src
             base._Process(delta);
             if (Input.IsActionJustPressed("pause_game"))
                 TogglePaused();
+            ButtonScaleTweenHover(ContinueButton, 1.3F, 0.3, 0.2);
+            ButtonScaleTweenHover(MainMenuButton, 1.3F, 0.3, 0.2);
+        }
+
+        private void ButtonScaleTweenHover(Button button, float scale, double duration, double revertDuration)
+        {
+            if (button.ButtonPressed)
+                Tween(button, "scale", Vector2.One * ((scale - 1) / 2 + 1), duration);
+            else if (button.IsHovered())
+                Tween(button, "scale", Vector2.One * scale, duration);
+            else
+                Tween(button, "scale", Vector2.One, revertDuration);
+        }
+
+        private void Tween(GodotObject obj, NodePath property, Variant amount, double duration)
+        {
+            var tween = CreateTween();
+            tween.TweenProperty(obj, property, amount, duration);
         }
     }
 }
